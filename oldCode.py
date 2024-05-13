83b9cbb6517ba45a0759fa26a70214450d876153
joshyii <joshyzh@gmail.com>
2024-05-13 18:39:03 +0800

    crossover, mutation done

---
+++ oldCode.py
@@ -0,0 +1,41 @@
+
+# pending to be deleted
+# missing = findMissing(parent)
+# missingInSecond = findMissing(second)
+# dupFirst = getDuplicatedElements(first)
+# dupSecond = getDuplicatedElements(second)
+# print(dupFirst)
+# print(dupSecond)
+
+# outFirst = []
+# for i in first:
+#     if i not in outFirst:
+#         outFirst.append(i)
+#     else:
+#         if(missingInFirst):
+#             outFirst.append(missingInFirst[0])
+#             missingInFirst.pop(0)
+
+# if (missingInFirst):
+#     outFirst += missingInFirst
+
+# print(outFirst)
+
+
+# outSecond = []
+# for i in second:
+#     if i not in outSecond:
+#         outSecond.append(i)
+#     else:
+#         if(missingInSecond):
+#             outSecond.append(missingInSecond[0])
+#             missingInSecond.pop(0)
+
+# if (missingInSecond):
+#     outSecond += missingInSecond
+
+
+# print(outSecond)
+
+# first = outFirst
+# second = outSecond
